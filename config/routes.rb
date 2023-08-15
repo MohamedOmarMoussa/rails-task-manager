2a070115d2e4b59fc174a3c61126fe24e87a8e6f
Mohamed Omar Moussa <mohamedomarmoussa02@gmail.com>
2023-08-15 12:52:04 +0200

    rail_task_manager

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,4 @@ Rails.application.routes.draw do
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch 'tasks/:id', to: 'tasks#update'
   delete 'tasks/:id', to: 'tasks#destroy'
-
-
 end
